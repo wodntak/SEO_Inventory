1b0a3a93b277bd49112fe5509caadce60bb9cda6
charmse <caleharms@protonmail.ch>
2018-03-12 10:08:58 -0500

    Update a lot

--- Code/seo.js
+++ Code/seo.js
@@ -1,3 +1,5 @@
+var global_email = '';
+var global_category = '';
 
 function Address(street, city, state, country, zip) {
     this.street = street;
@@ -34,13 +36,27 @@ function Item(name, qty, price, status, supplier) {
     this.supplier = supplier;
 }
 
+function setEmail(email) {
+    global_email = email; 
+}
+
+function getEmail() {
+    return global_email;
+}
+function setCategory(category) {
+    global_category = category
+}
+
+function getCategory() {
+    return global_category;
+}
+
 function login(email, password) {
     try {
-        getUser(email, password)
+        isUser(email, password);
         window.open('home.html', "_self");
-
-    }catch{
-        window.open('login.html', "_self")
+    }catch(err){
+        window.open('login.html', "_self");
     }
 }
 
@@ -48,12 +64,107 @@ function signup(email, password1, password2) {
     if ( (password1 == password2) && (password1 != '') && (password2 != '') && email.includes('@')) {
         user = new User(email, password1);
         createUser(user);
+        set
         window.open('userSettings.html', "_self");
     }else{
-        window.open("login.html", "_self");
+         window.open("login.html", "_self");
     }
 }
 
 function createUser(user) {
     //mysql insert
 }
+
+function isUser() {
+    return true;
+}
+
+function saveUserSettings(firstName, middleName, lastName, phoneNumber, notifications, street, city, state, zip, country) {
+    if(addressExists(street, city, state, zip, country)) {
+        addressId = getAddress(street, city, state, zip, country);
+    }else{
+        addressId = insertAddress(street, city, state, zip, country);
+    }
+    if(userInfoExists(firstName, middleName, lastName, phoneNumber, notifications)) {
+        updateUserInfo(firstName, middleName, lastName, phoneNumber, notifications);
+    }else{
+        insertUserInfo(firstName, middleName, lastName, phoneNumber, notifications);
+    }
+    window.open('userSettings.html', "_self");
+}
+
+function addressExists(street, city, state, zip, country) {
+    return true;
+}
+
+function getAddress(street, city, state, zip, country) {
+    return 0;
+}
+
+function insertAddress(street, city, state, zip, country) {
+    return 0;
+}
+
+function userInfoExists(firstName, middleName, lastName, phoneNumber, notifications) {
+    return true;
+}
+
+function updateUserInfo(firstName, middleName, lastName, phoneNumber, notifications) {
+
+}
+
+function insertUserInfo(firstName, middleName, lastName, phoneNumber, notifications) {
+
+}
+
+function saveCategory(name) {
+    //Update
+    if (categoryExists(name)) {
+        updateCategory(name);
+    }else{
+        insertCategory(name);
+    }
+}
+function categoryExists(name) {
+    return true;
+}
+
+function updateCategory(name) {
+    return;
+}
+
+function insertCategory(name) {
+    return;
+}
+
+function saveItem(name, price, qty, status, supplier) {
+    if (itemExists(name, price, qty, status, supplier)) {
+        updateItem(name, price, qty, status, supplier);
+    }else{
+        insertItem(name, price, qty, status, supplier);
+    }
+    window.open('home.html', "_self");
+}
+
+function itemExists(name, price, qty, status, supplier) {
+    return true;
+}
+
+function updateItem(name, price, qty, status, supplier) {
+    return;
+}
+
+function insertItem(name, price, qty, status, supplier) {
+    return;
+}
+
+function addQty(item) {
+    //update quantity q
+    window.open('home.html', "_self");
+}
+function subdQty(item) {
+    //update quantity q
+    oldQty = getQty(name, price, status, supplier)
+    window.open('home.html', "_self");
+}
+
